4bebf14c53d8c7a3d93933bebdb806152d9c7150
Arun Maurya <arunmaurya621@gmail.com>
2023-02-19 22:58:50 +0530

    Modified port settings

--- server/server.js
+++ server/server.js
@@ -3,8 +3,8 @@ const express=require('express')
 const app=express()
 const path=require('path')
 const cookieParser=require('cookie-parser')
-
-
+const dotenv=require('dotenv').config()
+const port=process.env.PORT || 9000
 
 
 app.use(cookieParser())
@@ -13,9 +13,9 @@ app.use(express.urlencoded())
 app.use(express.json())
 
 
-app.listen(9090,()=>{
+app.listen(port,()=>{
     console.log(__dirname)
-    console.log(`Running on 9090`)
+    console.log(`Running on ${port}...`)
 })
 app.use('/login',express.static(path.join(__dirname,'../client')))
 app.use('/register',express.static(path.join(__dirname,'../client')))
