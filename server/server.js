bc9122a4cbef00c6b2bab928d5315848c9dcba88
Arun Maurya <arunmaurya621@gmail.com>
2023-03-29 20:20:32 +0530

    Added favicon

--- server/server.js
+++ server/server.js
@@ -4,15 +4,18 @@ const app=express()
 const path=require('path')
 const cookieParser=require('cookie-parser')
 const dotenv=require('dotenv').config()
-const port=process.env.PORT || 9090
 
+const favicon = require('serve-favicon')
+app.use(favicon(path.join(__dirname, '../client/favicon.ico')))
+
+const port=process.env.PORT || 9090
 
 app.use(cookieParser())
 
 app.use(express.urlencoded())
 app.use(express.json())
 
-
+app.set('case sensitive routing ', true)
 app.listen(port,()=>{
     console.log(__dirname)
     console.log(`Running on ${port}...`)
