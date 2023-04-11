281c618f90fb031fe599c38542486b626110c2b3
Arun Maurya <arunmaurya621@gmail.com>
2023-04-11 13:48:21 +0530

    modified db vars

--- server/db/dbConn.js
+++ server/db/dbConn.js
@@ -1,20 +1,22 @@
-const mongoose=require('mongoose')
-const dotenv=require('dotenv').config()
+const mongoose = require('mongoose')
+const dotenv = require('dotenv').config()
 
-const dbLink=process.env.DB_LINK
+const dbLink = process.env.NODE_ENV === 'dev' ? process.env.DB_LINK_DEV : process.env.DB_LINK
+const dbName = process.env.NODE_ENV === 'dev' ? process.env.DB_NAME_DEV : process.env.DB_NAME
 
-const dbConn=mongoose.connect(dbLink,{
-    autoIndex:true,
-    dbName:process.env.DB_NAME
+const dbConn = mongoose.connect(dbLink, {
+    autoIndex: true,
+    dbName
 })
 
-dbConn.then(()=>{
+dbConn.then(() => {
     console.log('DB Connected')
 })
-dbConn.catch((err)=>{
+dbConn.catch((err) => {
+    console.log(dbLink)
     console.log(`- - - - - - - - - - - - - Couldn't Connect to Database- - - - - - - - - - - - - -\n`)
     console.log(err)
 })
 
 // module.exports=mongoClient
-module.exports=dbConn
+module.exports = dbConn
